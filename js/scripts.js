61da12c14ab086029d405b8f02d0ff6cf396051f
kohichi horii and nelia terrazas <nelia@>
2019-01-16 14:37:57 -0800

    Add input date type and selectors

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     var nameInput = $("input#name").val();
     var emailInput = $("input#email").val();
     var dateInput = $("input#date").val();
-    var requestInput = $("input#request").val();
+    var requestInput = $("select#request").val();
 
     $(".name").text(nameInput);
     $(".email").text(emailInput);
